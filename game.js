387f5f7bb132508f24ef15c7467491b38ed2f021
weeaboo <uiop134@bk.ru>
2025-08-29 01:12:22 +0500

    ADAWDAW

--- game.js
+++ game.js
@@ -199,8 +199,8 @@ const isMobile = (('ontouchstart' in window) || navigator.maxTouchPoints > 0);
 if (isMobile) {
   obstacleSpeed = Math.max(2.6, obstacleSpeed * 0.75);
   speedIncrease = Math.max(0.12, speedIncrease * 0.55);
-  OBSTACLE_Y_ADJUST = 2;
-  trackOffset = 135;
+  OBSTACLE_Y_ADJUST = 4;
+  trackOffset = 133;
 }
 function increaseSpeed() {
   if (obstacleSpeed < maxSpeed) {
