b6f01fb831e4f9c5b1dd422f8901959e037a9a92
weeaboo <uiop134@bk.ru>
2025-08-29 01:13:40 +0500

    awdawdawd

--- game.js
+++ game.js
@@ -199,6 +199,7 @@ const isMobile = (('ontouchstart' in window) || navigator.maxTouchPoints > 0);
 if (isMobile) {
   obstacleSpeed = Math.max(2.6, obstacleSpeed * 0.75);
   speedIncrease = Math.max(0.12, speedIncrease * 0.55);
+  BASE_TRACK_OFFSET = 133;
   OBSTACLE_Y_ADJUST = 4;
   trackOffset = 133;
   FIRE_RELATIVE_ADJUST = 5;
